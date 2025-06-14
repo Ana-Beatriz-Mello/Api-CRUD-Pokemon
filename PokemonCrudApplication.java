0a768547fee527d0cbef810189a38ed7a6a33e3c
Ana-Beatriz-Mello <114688794+Ana-Beatriz-Mello@users.noreply.github.com>
2025-06-14 12:25:55 -0300

    Create PokemonCrudApplication.java

---
+++ PokemonCrudApplication.java
@@ -0,0 +1,11 @@
+package com.example.PokemonCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCrudApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCrudApplication.class, args);
+	}
+}
